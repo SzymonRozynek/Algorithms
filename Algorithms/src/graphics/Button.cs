91be926ab57f10b1c6030d56c229cd6412629de2
dmistera2 <dmistera2@gmail.com>
2018-03-24 18:04:15 +0100

    Added arrows to switch tabs on the table.

---
+++ Algorithms/src/graphics/Button.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Window;
+using SFML.Graphics;
+
+namespace Algorithms {
+     class Button {
+
+        public Button(Texture tex) {
+            texture = tex;
+            Sprite = new Sprite(texture);
+        }
+
+        private Sprite sprite;
+        private Texture texture;
+        private bool clicked = false;
+
+        public Sprite Sprite { get => sprite; set => sprite = value; }
+
+        public void Update(OnClick onClick, RenderWindow window) {
+            if(Mouse.IsButtonPressed(Mouse.Button.Left) && !clicked && sprite.GetGlobalBounds().Contains(Mouse.GetPosition(window).X, Mouse.GetPosition(window).Y)) {
+                onClick();
+                clicked = true;
+            }
+            else if(!Mouse.IsButtonPressed(Mouse.Button.Left)) {
+                clicked = false;
+            }
+        }
+
+        public void Draw(RenderWindow w) {
+            w.Draw(sprite);
+        }
+
+        public delegate void OnClick();
+    }
+}
--- Algorithms/src/graphics/Table.cs
+++ Algorithms/src/graphics/Table.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using SFML.Graphics;
 using SFML.System;
+using SFML.Window;
 
 namespace Algorithms {
     class Table : Window {
@@ -16,6 +17,10 @@ namespace Algorithms {
         private int ySpace = 30;
         private int titleHeight = 100;
 
+        private Button leftButton;
+        private Button rightButton;
+        private int buttonOffset = 50;
+
         private Tab selectedTab;
 
         public Table(int width, int height) {
@@ -24,11 +29,20 @@ namespace Algorithms {
             windowHeight = (uint)((height + 1) * ySpace + titleHeight);
             tabs = new List<Tab>();
             font = new Font("fonts\\arial.ttf");
+            leftButton = new Button(new Texture("assets\\button.png"));
+            leftButton.Sprite.Scale *= 0.05f;
+            leftButton.Sprite.Position = new Vector2f(buttonOffset, titleHeight / 2);
+            leftButton.Sprite.Origin = new Vector2f(leftButton.Sprite.GetLocalBounds().Width / 2, leftButton.Sprite.GetLocalBounds().Height / 2);
+            leftButton.Sprite.Rotation = 180;
+            rightButton = new Button(new Texture("assets\\button.png"));
+            rightButton.Sprite.Scale *= 0.05f;
+            rightButton.Sprite.Position = new Vector2f(windowWidth - buttonOffset, titleHeight / 2);
+            rightButton.Sprite.Origin = new Vector2f(leftButton.Sprite.GetLocalBounds().Width / 2, leftButton.Sprite.GetLocalBounds().Height / 2);
         }
 
         public void AddTab(Tab tab) {
             tabs.Add(tab);
-            selectedTab = tab; //temp
+            selectedTab = tab;
         }
 
         protected override string GetWindowName() {
@@ -36,8 +50,36 @@ namespace Algorithms {
         }
 
         protected override void Update() {
+            leftButton.Update(TabLeft, renderWindow);
+            leftButton.Draw(renderWindow);
+            rightButton.Update(TabRight, renderWindow);
+            rightButton.Draw(renderWindow);
+            Draw();
+        }
 
-            for(int x = 1; x < width; x++) {
+        private void TabLeft() {
+            int i = tabs.IndexOf(selectedTab);
+            if(i == 0) {
+                i = tabs.Count - 1;
+            }
+            else {
+                i--;
+            }
+            selectedTab = tabs.ElementAt(i);
+        }
+
+        private void TabRight() {
+            int i = tabs.IndexOf(selectedTab);
+            if (i == tabs.Count - 1) {
+                i = 0;
+            } else {
+                i++;
+            }
+            selectedTab = tabs.ElementAt(i);
+        }
+
+        private void Draw() {
+            for (int x = 1; x < width; x++) {
                 RectangleShape r = new RectangleShape() {
                     Position = new Vector2f(((float)x / width) * windowWidth, titleHeight),
                     FillColor = Color.Yellow,
@@ -50,7 +92,7 @@ namespace Algorithms {
 
                 Text titleText = new Text(selectedTab.title, font) {
                     Position = new Vector2f(windowWidth / 2, titleHeight / 2),
-                    CharacterSize = (uint)(titleHeight/2)
+                    CharacterSize = (uint)(titleHeight / 2)
                 };
                 titleText.Origin = new Vector2f(titleText.GetLocalBounds().Width / 2, titleText.GetLocalBounds().Height / 2);
                 renderWindow.Draw(titleText);
