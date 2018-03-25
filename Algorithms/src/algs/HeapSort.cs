72c650617bfa8056d572510e1cf0abbdff8614b8
SzymonRozynek <szymonrozynek98@wp.pl>
2018-03-25 12:41:26 +0200

    Add files via upload

---
+++ Algorithms/src/algs/HeapSort.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Algorithms {
+    class HeapSort : SortingAlgorithm {
+
+        public override string GetName() {
+            return "Heap Sort";
+        }
+
+        public override bool IsInPlace()
+        {
+            return true;
+        }
+
+
+        void Heap(Element[] elements, int i, int end)
+        {
+            if (((2*i <= end) &&(elements[2 * i].value > elements[i].value))||( (2 * i + 1 <= end) && (elements[2 * i + 1].value > elements[i].value)))
+            {
+                if (elements[2 * i + 1].value > elements[2 * i].value)
+                {
+                    if (2 * i + 1 < end / 2)
+                    {
+                        SwapElements(elements, 2 * i + 1, i);
+                        Heap(elements, 2 * i + 1, end);
+                    }                  
+                }
+                else {
+                    if (2 * i < end / 2)
+                    {
+                        SwapElements(elements, 2 * i, i);
+                        Heap(elements, 2 * i, end);
+                    }
+                }
+               
+            }
+
+        }
+
+
+        protected override void SortElements(Element[] elements) {
+            int end = elements.Length - 1;
+            for(int i=end/2; i > 0; i++)
+            {
+                Heap(elements, i,end);
+                IterationTick();
+            }
+            while(end>1)
+            {
+                SwapElements(elements, 1, end);
+                Heap(elements, 1, --end);
+                IterationTick();
+            }
+
+        }
+
+    }
+}
---
+++ Algorithms/src/algs/QuickSort.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Algorithms {
+    class QuickSort : SortingAlgorithm
+    {
+
+        public override string GetName()
+        {
+            return "Quick Sort";
+        }
+
+        public override bool IsInPlace()
+        {
+            return true;
+        }
+
+        void QS(Element[] elements, int s, int e)
+        {
+            int p, l, r;
+            l = s;
+            r = e;
+            p = elements[l].value;
+            while (l < r)
+            {
+                while (elements[l].value <p) { l++; IterationTick(); }
+                while (elements[r].value >p) { r--; IterationTick(); }
+                if(l<r) SwapElements(elements, l++, r);
+            }
+
+            if (s < r ) QS(elements, s, r-1);
+            if (r < e) QS(elements, r+1, e);
+        }
+
+
+        protected override void SortElements(Element[] elements)
+        {
+            int start = 0;
+            int end = elements.Length - 1;
+            QS(elements, start, end);
+            for (int i = 1; i <= end; i++)
+            {
+                Console.WriteLine(elements[i].value);
+
+            } 
+        }
+    }
+ }
+
