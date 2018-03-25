052d45175db5f341fc9a371a6beac10d58436fcf
dmistera2 <dmistera2@gmail.com>
2018-03-25 21:55:09 +0200

    Fixed heap sort

--- Algorithms/src/algs/HeapSort.cs
+++ Algorithms/src/algs/HeapSort.cs
@@ -16,57 +16,6 @@ namespace Algorithms {
             return true;
         }
 
-
-        /*void Heap(Element[] elements, int i, int end)
-        {
-            int l, p;
-            if (2 * i <= end)
-            {
-                l = 2 * i;
-                if ((2 * i + 1) <= end)
-                {
-                    p = 2 * i;
-                    if ((elements[l].value > elements[p].value))
-                    {
-                        if (elements[l].value > elements[i].value)
-                        {
-                            SwapElements(elements, l, i);
-                            Heap(elements, l, end);
-                        }
-                    }
-                    else if (elements[p].value > elements[i].value)
-                    {
-                        SwapElements(elements, p, i);
-                        Heap(elements, p, end);
-                    }
-
-                }
-                if (elements[l].value > elements[i].value)
-                {
-                    SwapElements(elements, l, i);
-                    Heap(elements, l, end);
-                }
-            }
-
-        }
-
-
-        protected override void SortElements(Element[] elements) {
-            int end = elements.Length - 1;
-            for(int i=end/2; i > 0; i++)
-            {
-                Heap(elements, i,end);
-                IterationTick();
-            }
-            while(end>1)
-            {
-                SwapElements(elements, 1, end);
-                Heap(elements, 1, --end);
-                IterationTick();
-            }
-
-        }*/
-
         private void Heap(Element[] elements, int index, int max) {
             IterationTick();
             if(index * 2 + 2 <= max) {
@@ -98,8 +47,8 @@ namespace Algorithms {
                 Heap(elements, i, elements.Length -1);
             }
             for(int i = 0; i < elements.Length - 1; i++) {
-                SwapElements(elements, 0, elements.Length - 1 - i);
                 Heap(elements, 0, elements.Length - 1 - i);
+                SwapElements(elements, 0, elements.Length - 1 - i);
             }
         }
     }
